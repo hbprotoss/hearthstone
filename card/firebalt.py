e691748e7ea33367f5020756cba943dc07370c9e
hbprotoss <gamespy1991@gmail.com>
2017-10-06 10:50:03 +0800

    add Firebalt

---
+++ card/firebalt.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+from card import Power
+
+
+class Firebalt(Power):
+    def __init__(self):
+        Power.__init__(self)
+        self.attack = self.cur_attack = 1
+        self.target_count = 1
+
+    def post_play(self, target_cards):
+        assert len(target_cards) == 1
+        target = target_cards[0]
+        target.cur_health = target.cur_health - self.cur_attack
